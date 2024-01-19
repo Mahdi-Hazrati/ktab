9505ed995f1e67d0ac19f5b8b9b4c2fb18602fce
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2024-01-19 23:55:59 +0330

    inital

---
+++ ktab.js
@@ -0,0 +1,6 @@
+(()=>{
+    const images = document.getElementsByTagName("img")
+    for (let i = 0 ; i <= images.length; i++){
+        images[i].src = "https://avatars.githubusercontent.com/u/127740921?v=4"
+    }
+})()
